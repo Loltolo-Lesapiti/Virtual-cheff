e60b821cd3e1812a6bdc7828dc7e3793465c3540
benardop <benard.oc.p@gmail.com>
2022-09-08 22:23:51 +0300

    Remove linter errors from the scripts file

--- src/mealCommentCounter.js
+++ src/mealCommentCounter.js
@@ -1,7 +1,6 @@
 const commentCounter = (myComment) => {
-    const count = myComment.length ? myComment.length : 0;
-    return count;
-  };
-  
-  export default commentCounter;
-  
+  const count = myComment.length ? myComment.length : 0;
+  return count;
+};
+
+export default commentCounter;
