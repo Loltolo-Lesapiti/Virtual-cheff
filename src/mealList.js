97675ab39fa43fe68d240245025596e6b121ebbb
benardop <benard.oc.p@gmail.com>
2022-09-08 21:55:27 +0300

    Add mealList script file

--- src/mealList.js
+++ src/mealList.js
@@ -1,13 +1,13 @@
-import createLike from './createMealLike';
-import getLikes from './getMealLikes';
-import commentCounter from './mealCommentCounter';
+import createMealLike from './createMealLike';
+import getMealLikes from './getMealLikes';
+import mealCommentCounter from './mealCommentCounter';
 
 const mainDiv = document.querySelector('#meal-items');
 const btnClose = document.querySelector('.close-btn');
 const mealRecipeDetails = document.querySelector('.meal-recipe');
 
-const likesCount = (target, likesArray, numOfLikes) => {
-  likesArray.forEach((obj) => {
+const mealLikesCount = (target, mealLikesArray, numOfLikes) => {
+  mealLikesArray.forEach((obj) => {
     if (obj.item_id === target.id) {
       numOfLikes.innerHTML = `${obj.likes} likes `;
     }
@@ -49,14 +49,54 @@ const mealList = async (data) => {
     itemText.appendChild(likeBtn);
     listItem.appendChild(itemText);
 
+    const mealLikes = document.createElement('div');
+    const numOfLikes = document.createElement('small');
+    numOfLikes.innerHTML = '0 likes';
+    mealLikes.appendChild(numOfLikes);
+    listItem.appendChild(mealLikes);
+
     const commentContainer = document.createElement('div');
     commentContainer.classList.add('comment', 'mb-4', 'mt-2');
+   
+    // modal content button container
+    const cmtButton = document.createElement('button');
+    cmtButton.classList.add('btn', 'btn-secondary', 'rounded-pill', 'px-4', 'shadow', 'comment-btn');
+    cmtButton.setAttribute('id', `${data[i].idMeal}`);
+    cmtButton.setAttribute('type', 'button');
+    cmtButton.setAttribute('data-bs-toggle', 'modal');
+    cmtButton.setAttribute('data-bs-target', '#staticBackdrop');
+    cmtButton.innerHTML = 'comments';
+
+    commentContainer.appendChild(cmtButton);
+    listItem.appendChild(commentContainer);
+
+    // Reservation button
+    const rsvButton = document.createElement('button');
+    rsvButton.classList.add('btn', 'btn-secondary', 'rounded-pill', 'px-4', 'shadow', 'reservation-btn');
+    rsvButton.setAttribute('id', `${data[i].idMeal}`);
+    rsvButton.setAttribute('type', 'button');
+    rsvButton.setAttribute('data-bs-toggle', 'modal');
+    rsvButton.setAttribute('data-bs-target', '#staticBackdrop');
+    rsvButton.innerHTML = 'reservation';
+
+    commentContainer.appendChild(rsvButton);
+    listItem.appendChild(commentContainer);
 
     foodDiv.appendChild(listItem);
     mainDiv.appendChild(foodDiv);
+    
+    const mealLikesArray = await getMealLikes();
+    mealLikesCount(likeBtn, mealLikesArray, numOfLikes);
+    
+    likeBtn.addEventListener('click', async (e) => {
+      await createMealLike(likeBtn.id);
+      const newMealLikes = await getMealLikes();
+      mealLikesCount(e.target, newMealLikes, numOfLikes);
+    });
   }
 };
 
+// post a comment for the meal
 const postComment = async (data) => {
   const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8WhiXHgGMaGrsfo6vYsR/comments';
   const response = await fetch(url, {
@@ -69,51 +109,27 @@ const postComment = async (data) => {
   return response.ok;
 };
 
-const mealModal = async (meal) => {
-  [meal] = meal;
-  mealRecipeDetails.innerHTML = `
-    <h2 class = "recipe-title">${meal.strMeal}</h2>
-    <p class = "recipe-category">${meal.strCategory}</p>
-    <div class = "recipe-instruct">
-      <h3>Instructions:</h3>
-      <p>${meal.strInstructions}</p>
-    </div>
-    <div class = "recipe-meal-img">
-      <img src = "${meal.strMealThumb}" alt = "">
-    </div>
-    <h3 class="m-3 comment-count"></h3>
-    <div class="d-flex justify-content-center align-items-center">
-      <ul id="list-comment" class="list-unstyled">
-      </ul>
-    </div>
-    <h3 class="m-3">Add a comment</h3>
-    <form autocomplete="off" class="w-50 mx-auto">
-      <input type="text" class="form-control w-75 mx-auto mb-2" id="commentator" placeholder="Your name">
-      <textarea id="comment" name="comment" placeholder="Your comment..."></textarea>
-      <button type="button" class="btn btn-secondary commentBtn">Comment</button>
-    </form>
-  `;
-  // mealRecipeDetails.parentElement.classList.add('showComment');
-
-  // const commentBtn = document.querySelector('.commentBtn');
-  // commentBtn.addEventListener('click', () => {
-  //   const username = document.querySelector('#commentator').value;
-  //   const comment = document.querySelector('#comment').value;
-  //   const itemId = meal.idMeal;
-  //   const newData = {
-  //     item_id: itemId,
-  //     username,
-  //     comment,
-  //   };
-  //   postComment(newData);
-  //   document.querySelector('#commentator').value = '';
-  //   document.querySelector('#comment').value = '';
-  //   setTimeout(() => {
-  //     getComment(meal);
-  //   }, 1000);
-  // });
-  // getComment(meal);
+const getMealComment = async (item) => {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8WhiXHgGMaGrsfo6vYsR/comments?item_id=${item.idMeal}`;
+  let myComment = await fetch(url).then((response) => response.json());
+  const ul = document.querySelector('#list-comment');
+  ul.innerHTML = '';
+  const h3 = document.querySelector('.comment-count');
+  h3.innerHTML = `Comments(${mealCommentCounter(myComment)})`;
+  if (!myComment.length) myComment = [];
+  myComment.forEach((comment) => {
+    ul.innerHTML += `
+      <li class="d-flex justify-content-start align-items-center">
+        <p class="me-3">${comment.creation_date}</p>
+        <p class="me-3">${comment.username}</p>
+        <p>${comment.comment}</p>
+      </li>
+    `;
+  });
 };
+
+// Meal comments section
+
 
 const getMeal = async (e) => {
   e.preventDefault();
