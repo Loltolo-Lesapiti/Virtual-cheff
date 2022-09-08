89d4f639f420caf5b019752a9ac60203dfd1f390
benardop <benard.oc.p@gmail.com>
2022-09-08 22:24:05 +0300

    Remove linter errors from the scripts file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
-import getMeals from './getMeals';
-import mealList from './mealList';
+import getMeals from './getMeals.js';
+import mealList from './mealList.js';
 
 window.addEventListener('load', async () => {
-    mealList(await getMeals());
+  mealList(await getMeals());
 });
